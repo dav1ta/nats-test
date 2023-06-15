7b215fffafa4f71f3099a0f1cb4578eecfa34da3
dav1ta <daviti.tchanturia.1@btu.edu.ge>
2023-06-15 20:09:46 +0400

    simple siem

--- publisher/main.go
+++ publisher/main.go
@@ -1,83 +1,74 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "math/rand"
-    "os"
-    "os/signal"
-    "syscall"
-    "time"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-    "github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go"
 )
 
-const (
-    Letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-)
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-func RandStringBytes(n int, charset string) string {
-    b := make([]byte, n)
-    for i := range b {
-        b[i] = charset[rand.Intn(len(charset))]
-    }
-    return string(b)
+func generateRandomString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
 }
 
-func RandomIP() string {
-    return fmt.Sprintf("%d.%d.%d.%d", rand.Intn(255), rand.Intn(255), rand.Intn(255), rand.Intn(255))
+func generateRandomIP() string {
+	return fmt.Sprintf("%d.%d.%d.%d", rand.Intn(255), rand.Intn(255), rand.Intn(255), rand.Intn(255))
 }
 
-func GenerateNewSyslog() string {
-    timestamp := time.Now().Format("Mon Jan _2 15:04:05 2006")
-    hostname := RandStringBytes(5, Letters)
-    firewall := RandStringBytes(5, Letters)
-    srcIP := RandomIP()
-    dstIP := RandomIP()
+func generateSyslogMessage() string {
+	timestamp := time.Now().Format("Mon Jan _2 15:04:05 2006")
+	hostname := generateRandomString(5)
+	firewall := generateRandomString(5)
+	srcIP := generateRandomIP()
+	dstIP := generateRandomIP()
 
-    return fmt.Sprintf("%s: <14>Mar  4 15:53:03 %s %s/box_Firewall_Activity:  Info     %s Remove: type=FWD|proto=UDP|srcIF=eth1|srcIP=%s|srcPort=35119|srcMAC=08:00:27:da:d7:9c|dstIP=%s|dstPort=53|dstService=domain|dstIF=eth0|rule=InternetAccess/<App>:RestrictTim|info=Balanced Session Idle Timeout|srcNAT=%s|dstNAT=%s|duration=21132|count=1|receivedBytes=130|sentBytes=62|receivedPackets=1|sentPackets=1|user=|protocol=|application=|target=|content=|urlcat",
-        timestamp, hostname, firewall, firewall, srcIP, dstIP, srcIP, dstIP)
+	return fmt.Sprintf("%s: <14>Mar  4 15:53:03 %s %s/box_Firewall_Activity:  Info     %s Remove: type=FWD|proto=UDP|srcIF=eth1|srcIP=%s|srcPort=35119|srcMAC=08:00:27:da:d7:9c|dstIP=%s|dstPort=53|dstService=domain|dstIF=eth0|rule=InternetAccess/<App>:RestrictTim|info=Balanced Session Idle Timeout|srcNAT=%s|dstNAT=%s|duration=21132|count=1|receivedBytes=130|sentBytes=62|receivedPackets=1|sentPackets=1|user=|protocol=|application=|target=|content=|urlcat",
+		timestamp, hostname, firewall, firewall, srcIP, dstIP, srcIP, dstIP)
 }
 
 func main() {
-    rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 
-    // Connect to NATS
-    nc, err := nats.Connect("nats://nats:4222")
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Connect to NATS
+	nc, err := nats.Connect("nats://nats:4222")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // Access JetStream
-    js, err := nc.JetStream()
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Access JetStream
+	js, err := nc.JetStream()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // Create a quit channel
-    quit := make(chan os.Signal, 1)
-    signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	// Create a quit channel
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
 
-    // start a go routine to generate syslog messages and publish them to NATS
-    go func() {
-        for {
-            select {
-            case <-quit:
-                return
-            default:
-                msg := GenerateNewSyslog()
-                _, err := js.Publish("SYSLOGS.sources", []byte(msg))
-                if err != nil {
-                    log.Fatal(err)
-                }
-            }
-        }
-    }()
-
-    // Wait for the interrupt signal
-    <-quit
-
-    // Drain the connection (waits for pending messages to be published)
-    nc.Drain()
+	for {
+		select {
+		case <-quit:
+			// Drain the connection (waits for pending messages to be published)
+			nc.Drain()
+			return
+		default:
+			msg := generateSyslogMessage()
+			_, err := js.Publish("SYSLOGS.sources", []byte(msg))
+      // log.Printf("Published: %s", msg)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
 }
-
--- subscriber/main.go
+++ subscriber/main.go
@@ -4,16 +4,60 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"sync/atomic"
+	"regexp"
+	"strconv"
 	"syscall"
-	"time"
-
+  "time"
 	"github.com/nats-io/nats.go"
 )
 
+
+
+
+type SyslogEvent struct {
+	EventID         string
+	EventStatus     string
+	DeviceName      string
+	Protocol        string
+	SrcIP           string
+	DestIP          string
+	Rule            string
+	Duration        int
+	Count           int
+	ReceivedBytes   int
+	SentBytes       int
+	ReceivedPackets int
+}
+
+type Rule struct {
+    Name        string
+    Description string
+    Condition   func(e SyslogEvent) bool
+}
 func main() {
 	// Connect to NATS
-  time.Sleep(10 * time.Second)
+
+var deviceFailures = make(map[string]int)
+
+    rules := []Rule{
+      {
+        Name:        "Potential Brute Force Attack",
+        Description: "More than 5 login failures on a device within a minute",
+        Condition: func(e SyslogEvent) bool {
+          if e.EventID == "LOGIN" && e.EventStatus == "FAILURE" {
+            deviceFailures[e.DeviceName]++
+            if deviceFailures[e.DeviceName] > 2 {
+              // Reset the failure count after triggering the alert
+              deviceFailures[e.DeviceName] = 0
+              return true
+            }
+          }
+          return false
+        },
+      },
+    }
+
+
 	nc, err := nats.Connect("nats://nats:4222")
 	if err != nil {
 		log.Fatalf("Error connecting to NATS: %v", err)
@@ -26,24 +70,38 @@ func main() {
 	}
 
 	// Setup a durable, concurrent subscriber on the "SYSLOGS.sources" subject.
-	// The "nats" argument specifies the queue group.
-	var count int64
-	sub, err := js.QueueSubscribe("SYSLOGS.sources", "nats", func(msg *nats.Msg) {
-		atomic.AddInt64(&count, 1)
+	sub, err := js.Subscribe("SYSLOGS.sources", func(msg *nats.Msg) {
+		re := regexp.MustCompile(`.* (\S+)\/.*type=(\S+)\|.*srcIP=(\S+)\|.*dstIP=(\S+)\|.*rule=(\S+)\|.*duration=(\d+)\|.*count=(\d+)\|.*receivedBytes=(\d+)\|.*sentBytes=(\d+)\|.*receivedPackets=(\d+).*`)
+		match := re.FindStringSubmatch(string(msg.Data))
+
+		if len(match) == 0 {
+			log.Println("No match found in message")
+			return
+		}
+
+		event := SyslogEvent{}
+		event.DeviceName = match[1]
+		event.Protocol = match[2]
+		event.SrcIP = match[3]
+		event.DestIP = match[4]
+		event.Rule = match[5]
+		event.Duration, _ = strconv.Atoi(match[6])
+		event.Count, _ = strconv.Atoi(match[7])
+		event.ReceivedBytes, _ = strconv.Atoi(match[8])
+		event.SentBytes, _ = strconv.Atoi(match[9])
+		event.ReceivedPackets, _ = strconv.Atoi(match[10])
+
+		log.Printf("Received event: %+v", event)
+    for _, rule := range rules {
+        if rule.Condition(event) {
+            log.Printf("Alert! Rule '%s' triggered: %s", rule.Name, rule.Description)
+        }
+}
 	})
+
 	if err != nil {
 		log.Fatalf("Error subscribing to SYSLOGS.sources: %v", err)
 	}
-	defer sub.Unsubscribe() // Unsubscribe when done.
-
-	// Setup a ticker to print the number of messages received every second.
-	ticker := time.NewTicker(1 * time.Second)
-	go func() {
-		for range ticker.C {
-			val := atomic.SwapInt64(&count, 0)
-			log.Printf("Processed %d messages in the last second", val)
-		}
-	}()
 
 	// Setup a channel to handle OS signals to gracefully shut down
 	// when an interrupt signal is received.
@@ -54,6 +112,15 @@ func main() {
 	// Cleanup when done. Unsubscribe to the subject.
 	sub.Unsubscribe()
 	nc.Drain()
+
+
+  go func() {
+    for range time.Tick(1 * time.Minute) {
+      for device := range deviceFailures {
+        deviceFailures[device] = 0
+      }
+    }
+  }()
 
 	log.Println("Connection to NATS closed.")
 }
